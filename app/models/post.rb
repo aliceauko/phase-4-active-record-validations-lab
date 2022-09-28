0d7e822668fee6f20b474d694c596d41d30640c3
aliceauko <alice.auko@student.moringaschool.com>
2022-09-28 15:36:39 +0300

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :clickbait_y?
+
+
+    def clickbait_y?
+        unless ((/Won't Believe/i.match title) || (/Secret/i.match title) || (/Top \d/i.match title) || (/Guess/i.match title)) 
+            errors.add(:title, "must be clickbait_y")
+        end
+    end
+
 end
